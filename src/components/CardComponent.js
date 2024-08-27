8469d60869700e52aeb557115b82cfed8d0779d1
NigithaaG <152292540+NigithaaG@users.noreply.github.com>
2024-08-27 09:55:47 +0530

    Update CardComponent.js

--- src/components/CardComponent.js
+++ src/components/CardComponent.js
@@ -1,17 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const CardComponent = ({ dress }) => {
+const CardComponent = ({ dress, onAddToCart }) => {
   return (
-    <div className="col-md-4 mb-4">
-      <div className="card">
-        <img className="card-img-top" src={dress.image} alt={dress.name} />
-        <div className="card-body">
-          <h5 className="card-title">{dress.name}</h5>
-          <p className="card-text">{dress.description}</p>
-        </div>
+    <div className="card" style={{ width: '18rem', margin: '15px' }}>
+      <img src={dress.image} className="card-img-top" alt={dress.name} />
+      <div className="card-body">
+        <h5 className="card-title">{dress.name}</h5>
+        <p className="card-text">{dress.description}</p>
+        <button className="btn btn-primary" onClick={() => onAddToCart(dress)}>
+          Add to Cart
+        </button>
+        <button className="btn btn-secondary" style={{ marginLeft: '10px' }}>
+          Order
+        </button>
       </div>
     </div>
   );
+};
+
+CardComponent.propTypes = {
+  dress: PropTypes.object.isRequired,
+  onAddToCart: PropTypes.func.isRequired,
 };
 
 export default CardComponent;
