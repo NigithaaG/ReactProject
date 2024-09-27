bb351f4f0d166b0391d2b4fff801d8c7ccc5b676
NigithaaG <152292540+NigithaaG@users.noreply.github.com>
2024-09-27 16:06:02 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
 import LandingPageComponent from './components/LandingPageComponent';
 import LoginContainer from './containers/LoginContainer';
 import HomeContainer from './containers/HomeContainer';
+import DressListComponent from './components/DressListComponent';
+import CartComponent from './components/CartComponent';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LandingPageComponent />} />
-        <Route path="/login" element={<LoginContainer />} />
-        <Route path="/home" element={<HomeContainer />} />
-      </Routes>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <div className="container-fluid">
+            <Link className="navbar-brand" to="/">Home</Link>
+            <div className="navbar-nav">
+              <Link className="nav-link" to="/dresses">Dresses</Link>
+              <Link className="nav-link" to="/cart">Cart</Link>
+            </div>
+          </div>
+        </nav>
+        <Routes>
+          <Route path="/" element={<LandingPageComponent />} />
+          <Route path="/login" element={<LoginContainer />} />
+          <Route path="/home" element={<HomeContainer />} />
+          <Route path="/dresses" element={<DressListComponent />} />
+          <Route path="/cart" element={<CartComponent />} />
+        </Routes>
+      </Router>
+    </Provider>
   );
 }
 
