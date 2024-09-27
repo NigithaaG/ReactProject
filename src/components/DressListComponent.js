8d02f716bf0fa886733893506d733649b26a0693
NigithaaG <152292540+NigithaaG@users.noreply.github.com>
2024-09-27 16:05:50 +0530

    Update DressListComponent.js

--- src/components/DressListComponent.js
+++ src/components/DressListComponent.js
@@ -1,51 +1,34 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import CardComponent from './CardComponent';
+import { fetchDresses, addToCart } from '../store';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-class DressListComponent extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      dresses: [],
-    };
-  }
+const DressListComponent = () => {
+  const dispatch = useDispatch();
+  const dresses = useSelector(state => state.dresses.list);
 
-  componentDidMount() {
-    this.fetchDresses();
-  }
+  useEffect(() => {
+    dispatch(fetchDresses());
+  }, [dispatch]);
 
-  fetchDresses() {
-    const dresses = [
-      { name: 'Blue Regular Maxi dress', description: 'Rs.699', image: '/images/n2.avif' },
-      { name: 'Blue Floral print dress', description: 'Rs.549', image: '/images/n4.webp' },
-      { name: 'Green Embroidered dress', description: 'Rs.650', image: '/images/n4.avif' },
-      { name: 'Pink Floral Churidhar', description: 'Rs.750', image: '/images/n3.jpg' },
-    ];
-    this.setState({ dresses });
-  }
-
-  handleAddToCart = (dress) => {
+  const handleAddToCart = (dress) => {
+    dispatch(addToCart(dress));
     toast.success(`${dress.name} added to cart!`);
   };
 
-  render() {
-    return (
-      <div className="container">
-        <h2>Our Collection</h2>
-        <div className="row">
-          {this.state.dresses.map((dress, index) => (
-            <CardComponent 
-              key={index} 
-              dress={dress} 
-              onAddToCart={this.handleAddToCart} 
-            />
-          ))}
-        </div>
-        <ToastContainer />
+  return (
+    <div className="container">
+      <h2>Our Collection</h2>
+      <div className="row">
+        {dresses.map((dress) => (
+          <CardComponent key={dress.id} dress={dress} onAddToCart={handleAddToCart} />
+        ))}
       </div>
-    );
-  }
-}
+      <ToastContainer />
+    </div>
+  );
+};
 
 export default DressListComponent;
